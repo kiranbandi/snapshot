d8ff1772ac1947004b2d490c4542f9a146b2715c
kiranbandi <bvenkatkiran@gmail.com>
2020-07-15 03:01:13 -0600

    removed unecessary babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'src/index.js',
@@ -10,5 +9,5 @@ export default {
         format: 'iife',
         name: 'snapshot'
     },
-    plugins: [babel(), resolve(), commonjs()]
+    plugins: [resolve(), commonjs()]
 };
